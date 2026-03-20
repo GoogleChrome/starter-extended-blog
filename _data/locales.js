88eeb5a60989fd2a63e360c3801dc15ce5e3fb30
Thomas Steiner <tomac@google.com>
2026-03-20 16:55:45 +0100

    Initial commit

---
+++ _data/locales.js
@@ -0,0 +1,9 @@
+export const locales = ['en', 'es', 'ja', 'en-US', 'en-GB', 'es-ES', 'es-US'];
+
+export const fallbackLocales = {
+  'en-US': 'en',
+  'en-GB': 'en',
+  'es-ES': 'es',
+  'es-US': 'es',
+  '404.html': 'en',
+};
